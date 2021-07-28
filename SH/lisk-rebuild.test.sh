02853f885ac4786567a7d5d0bb5ec6299ff97634
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-07-28 02:35:44 -0400

    Update lisk-rebuild.test.sh

--- SH/lisk-rebuild.test.sh
+++ SH/lisk-rebuild.test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Gr33nDrag0n
+# https://github.com/Gr33nDrag0n69/LiskCore3Tools
+
 URL="https://testnet3-snapshot.lisknode.io/blockchain.db.tar.gz"
 lisk-core blockchain:download --url $URL --output ~/
 pm2 stop lisk-core --silent && sleep 3
