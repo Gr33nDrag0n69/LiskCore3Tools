da38f97e80a2126a9db058d5e358354616eb6ee8
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-08-28 23:24:29 -0400

    Update lisk-rebuild.main.sh

--- SH/lisk-rebuild.main.sh
+++ SH/lisk-rebuild.main.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-# Gr33nDrag0n
+# Gr33nDrag0n - v1.1 2021/08/28
 # https://github.com/Gr33nDrag0n69/LiskCore3Tools
 
-URL="https://snapshot.lisknode.io/blockchain.db.tar.gz"
+select DOMAIN in lisk.io lisknode.io liskworld.info
+do
+    case $DOMAIN in
+        "lisk.io")
+            URL="https://snapshots.lisk.io/main/blockchain.db.tar.gz"
+            break
+            ;;
+
+        "lisknode.io")
+            URL="https://snapshot.lisknode.io/blockchain.db.tar.gz"
+            break
+            ;;
+
+        "liskworld.info")
+            URL="https://snapshot.liskworld.info/blockchain.db.tar.gz"
+            break
+            ;;
+    esac
+done
+
 lisk-core blockchain:download --url $URL --output ~/
 pm2 stop lisk-core --silent && sleep 3
 lisk-core blockchain:import ~/blockchain.db.tar.gz --force
